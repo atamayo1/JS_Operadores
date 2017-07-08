5f750d01929120860addd01822e41f418e4abe16
Anthony Tamayo Ortega <anthonytamayoortega@MacBook-Pro-de-Anthony.local>
2017-07-08 11:10:26 -0500

    Operadores aritméticos terminado

---
+++ javaScript/operadores.js
@@ -0,0 +1,19 @@
+//Operadores aritmeticos
+var a=8;
+var b=10;
+var c=3.5*(a+b)*4;
+//Operadores de comparación
+/*if(a<b){
+	alert("'a' es menor a 'b'");
+}else{
+	alert("'b' es menor o igual a 'a'");
+}*/
+
+//Operadores lógicos
+var tengoTiempo = true;
+var tengoDinero = true;
+if(tengoTiempo && tengoDinero){
+	alert("voy al cine");
+}else{
+	alert("veo la tele");
+}
